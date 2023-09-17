a13e1eb56f556eafb71f7c4dbbfb16546a316662
Vadim Toma <https://github.com/TomaVadim>
2023-09-17 22:36:09 +0300

    feat:   -removed swiper from dev dependencies   -changed & to unicode in index.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,28 @@
-import * as flsFunctions from "./modules/functions.js";
+import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-const swiper = new Swiper();
-
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu-burger');
-// burger ///
 
-function showBurgerMenu({target}) {
-    if (target.closest('.burger')) {
+function showBurgerMenu({ target }) {
+  if (target.closest('.burger')) {
+    burger.classList.toggle('_active');
+    menu.classList.toggle('_active-menu');
 
-        burger.classList.toggle('_active');
-        menu.classList.toggle('_active-menu');
-
-        document.body.classList.toggle('_hidden');
-    }
+    document.body.classList.toggle('_hidden');
+  }
 }
 
 burger.addEventListener('click', showBurgerMenu);
-
-// footer-active-menu //
 
 const footerLabels = document.querySelectorAll('.footer__label');
 const footersMenu = document.querySelectorAll('.menu-footer');
 
 for (let i = 0; i < footerLabels.length; i++) {
-    const activeList = () => {
+  const activeList = () => {
     footersMenu[i].classList.toggle('_list');
-}
+  };
 
-    footerLabels[i].addEventListener('click', activeList);
+  footerLabels[i].addEventListener('click', activeList);
 }
